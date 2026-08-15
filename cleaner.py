eacaddc0c2d0fbd1ce0eadf1cf8b4b830479b39f
aronvillarnobo <aronjayd899@gmail.com>
2026-08-15 00:42:19 -0300

    Initial commit: report generator with dynamic file merging

--- cleaner.py
+++ cleaner.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
+
+
 def file_cleaner(input_path, output_path):
+    
+
     if input_path.endswith(".csv"):
         df = pd.read_csv(input_path)
     else:
@@ -8,8 +12,8 @@ def file_cleaner(input_path, output_path):
 
     df.columns = df.columns.str.strip()
 
-    for columna in df.select_dtypes(include="str").columns:  
-        df[columna] = df[columna].str.strip()
+    for columns in df.select_dtypes(include="str").columns:  
+        df[columns] = df[columns].str.strip()
 
     df = df.dropna(how="all")
     df = df.drop_duplicates()
@@ -21,6 +25,5 @@ def file_cleaner(input_path, output_path):
         df.to_excel(output_path, index=False)
     print(f"new file saved as {output_path}")
 
-
-file_cleaner("dirty_data.csv", "clean_data.csv")
-# file_cleaner("dirty_data.xlsx", "clean_data.xlsx")
+    return output_path
+    
